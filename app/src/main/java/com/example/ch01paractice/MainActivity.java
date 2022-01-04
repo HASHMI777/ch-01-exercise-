b5a02ab4e92e051de4b14bf0dfa83ff9b132e38a
anas <syedanasalihashmi57@gmail.com>
2022-01-04 10:20:25 +0500

    chapter 01 paractice

---
+++ app/src/main/java/com/example/ch01paractice/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.ch01paractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text=findViewById(R.id.text);
+        text.setText("Good Morning");
+    }
+}
